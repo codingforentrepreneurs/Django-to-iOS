d88bd5a9eca12a048b9dd636b7961ef7132887a7
Justin Mitchel <jmitchel3@gmail.com>
2015-06-15 07:33:13 -0700

    9 - Using an Auth Header with Alamofire

--- Source/ios/srvup/ViewController.swift
+++ Source/ios/srvup/ViewController.swift
@@ -13,14 +13,40 @@ import KeychainAccess
 
 class ViewController: UIViewController {
     let authTokenUrl = "http://127.0.0.1:8000/api/auth/token/"
+    let projectsURL = "http://127.0.0.1:8000/api2/projects/?format=json"
     let keychain = Keychain(service: "com.codingforentrepreneurs.srvup")
+    
+    let tokenUse = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImptaXRjaGVsMyIsInVzZXJfaWQiOjEsImVtYWlsIjoiY29kaW5nZm9yZW50cmVwcmVuZXVyc0BnbWFpbC5jb20iLCJleHAiOjE0MzQ0MDgwNzJ9.5EvVwnrPwhSXrZtwTSwe-Bhs6vqPV_c4HZfqXoYc3iw"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let params = ["username": "jmitchel3", "password": 123]
-        var rTest = Alamofire.request(Method.POST, self.authTokenUrl, parameters: params)
         
-        rTest.responseJSON(options: nil, completionHandler: isComplete)
+        
+        
+        let url = NSURL(string: self.projectsURL)
+        var mutableURLRequest = NSMutableURLRequest(URL:url!)
+        mutableURLRequest.setValue("JWT \(self.tokenUse)", forHTTPHeaderField: "Authorization")
+        mutableURLRequest.HTTPMethod = "GET"
+        var manager = Alamofire.Manager.sharedInstance
+        
+        var getProjects = manager.request(mutableURLRequest)
+        
+        getProjects.responseJSON(options: nil, completionHandler:projectsReceived)
+        
+        
+//        var authToken = Alamofire.request(Method.POST, self.authTokenUrl, parameters: params)
+//        
+//        authToken.responseJSON(options: nil, completionHandler: isComplete)
+    }
+    
+    func projectsReceived(request:NSURLRequest, response:NSHTTPURLResponse?, data:AnyObject?, error:NSError?) -> Void {
+        
+        let statusCode = response!.statusCode
+        println(statusCode)
+        println(data)
+        
     }
     
     func isComplete(request:NSURLRequest, response:NSHTTPURLResponse?, data:AnyObject?, error:NSError?) -> Void {
@@ -43,7 +69,7 @@ class ViewController: UIViewController {
                 self.keychain["token"] = nil
             }
             
-            // println(self.keychain["token"])
+            println(self.keychain["token"])
             
         case 400...499:
             println("Server responded no")
